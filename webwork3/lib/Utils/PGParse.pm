5f09e6f94132e1ecf9a58df556b8bfa5ab93d647
Peter Staab <peter.staab@gmail.com>
2015-07-27 07:20:19 -0400

    making the PG::parse a Moo object.

--- webwork3/lib/Utils/PGParse.pm
+++ webwork3/lib/Utils/PGParse.pm
@@ -5,13 +5,19 @@ package Utils::PGParse;
 # input: a file path to a pg file
 # output: a LibraryProblem object containing tag data and other information about the file
 
-
+use Moo;
+use MooX::Types::MooseLike::Base qw(Str InstanceOf);
 use Data::Dump qw/dd/;
 use List::MoreUtils qw/any first_index/;
+use Path::Class;
 
 require Models::Library::Problem;
 use Models::Library::TextbookProblem;
 use Models::Library::DBinfo;
+
+has library_dir => (is => 'rw', isa => Str, required => 1);
+has problem_path => (is=>'rw', isa => Str, required => 1);
+has problem => (is => 'rw', isa => InstanceOf['Models::Library::Problem']);
 
 my @basic_tags = qw/DBsubject DBchapter DBsection Institution Date Author Email MLT MLTleader KEYWORDS Level Language Static MO Status/;
 my @numbered_tags = qw/TitleText AuthorText EditionText Section Problem/;
@@ -20,28 +26,22 @@ my $chapterSectionREGEX = qr{^(\d+)(\.(\d+))?$};
 my $integerREGEX = qr{^\s*(-?\d+)\s*$};
 
 sub parse {
-  my $filepath = shift;
-  $tb_links = shift;
+    my $self = shift; 
   
-  my $prob = Models::Library::Problem->new(path=>$filepath);
-  open my $file, $filepath or die "Could not open $file: $!";
-  parseTags($file,$prob);
-  close $file;
-    
-  open my $file, $filepath or die "Could not open $file: $!";
-  $prob->statement(parseStatement($file,$prob));
-  close $file;
-  
-  
-  
-  return $prob;
 
+    my $relPath = file($self->problem_path)->relative(dir($self->library_dir));
+    $self->problem(Models::Library::Problem->new(path=>$relPath->stringify)); 
+
+    $self->parseTags();
+    $self->parseStatement();
+
+    return $self->problem;
 }
 
 
 
 sub parseTags {
-    my ($file,$prob) = @_;
+    my $self = shift; 
   
     my $docregex = qr{DOCUMENT()};
     my $tagregex = qr{^\s*\#+\s*([a-zA-Z]+)\((.*)\)\s*$};
@@ -51,7 +51,10 @@ sub parseTags {
     my @textProblemInfo = ();  # store the information for each textbook problem tag
     my $author_info = {};
     my $db_info = {};
-    while(my $line = <$file>)  {
+    my @matches2;
+    my $FILE;
+    open $FILE, $self->problem_path or die "Could not open $FILE: $!";
+    while(my $line = <$FILE>)  {
         if(my @matches = $line =~ /$tagregex/i){
             my $tag = $matches[0];
             # check for matching the basic tag
@@ -73,16 +76,16 @@ sub parseTags {
                 elsif($tag eq "DBchapter"){   $db_info->{DBchapter} = $value; }
                 elsif($tag eq "DBsection"){   $db_info->{DBsection} = $value; }
                 elsif($tag eq "Institution"){ $author_info->{institution} = $value;}
-                elsif($tag eq "Date"){        $prob->date($value);}
+                elsif($tag eq "Date"){        $self->problem->date($value);}
                 elsif($tag eq "Email"){       $author_info->{email} = $value;}
                 elsif($tag eq "Author"){     # split the problem author into first and last names
                     my @els = split($splitLastSpaceregex, $value);
                     $author_info->{lastname} = $els[1] if $els[1];
                     $author_info->{firstname} = $els[0] if $els[0]; }
-                elsif($tag eq "MLT"){         $prob->mlt($value);}
-                elsif($tag eq "MLTleader"){   $prob->mlt_leader($value);}
-                elsif($tag eq "Level"){       $prob->level($value);}
-                elsif($tag eq "MO"){          $prob->MO($value);}
+                elsif($tag eq "MLT"){         $self->problem->mlt($value);}
+                elsif($tag eq "MLTleader"){   $self->problem->mlt_leader($value);}
+                elsif($tag eq "Level"){       $self->problem->level($value);}
+                elsif($tag eq "MO"){          $self->problem->MO($value);}
                 elsif($tag eq "KEYWORDS"){
                     my @keywords = $value =~ m/'([^']+)'/g;
                     my @kws = ();
@@ -90,11 +93,11 @@ sub parseTags {
                     for my $keyword (@keywords) {
                         push(@kws,split(",",$keyword));
                     }
-                    $prob->keywords([@kws]);
+                    $self->problem->keywords([@kws]);
                 }
             }
       # check for matching a numbered tag
-        } elsif(my @matches = $line =~ /$ntagregex/i){
+        } elsif(@matches = $line =~ /$ntagregex/i){
             my $tag = $matches[0];
             my $match = any { $_ =~ /$tag/i } @numbered_tags;
             if ($match){
@@ -123,14 +126,13 @@ sub parseTags {
                 }
             }
         }
-        
     }
     
-    $prob->keywords([]) unless $prob->keywords; ## make sure that the keywords is an array, empty if needed. 
+    $self->problem->keywords([]) unless $self->problem->keywords; ## make sure that the keywords is an array, empty if needed. 
     
     # This parses the textbook information
     my @tmp = map { parseTextbook($_) } @textProblemInfo;
-    my @tmp = grep { defined($_) } @tmp;
+    @tmp = grep { defined($_) } @tmp;
     
     #dd "These are the textbooks";
     #dd @tmp;
@@ -140,33 +142,36 @@ sub parseTags {
     for my $key (keys(%$author_info)){
         $info->{$key} = $author_info->{$key} if defined($author_info->{$key});
     }
-    $prob->problem_author(Models::Library::ProblemAuthor->new($info));
+    $self->problem->problem_author(Models::Library::ProblemAuthor->new($info));
     my $DBinfo = Models::Library::DBinfo->new($db_info);
-    $prob->DBinfo($DBinfo);
-    $prob->textbookProblems(\@tmp);
+    $self->problem->DBinfo($DBinfo);
+    $self->problem->textbookProblems(\@tmp);
     
-    
+    close $FILE;
 }
 
 sub parseStatement {
-    my ($file,$prob) = @_;
+    my $self = shift; 
     my $statement = "";
     my $found = "";
     my $inclPGregex = qr{includePGproblem};
-    while(my $line = <$file>)  {
+    my $FILE;
+    
+    open $FILE, $self->problem_path or die "Could not open $FILE: $!";
+    while(my $line = <$FILE>)  {
         if ($line =~ $inclPGregex){
-          $prob->isLink(1);
+          $self->problem->isLink(1);
         } elsif (my @matches = $line =~ /^\s*(TEXT\(EV2\(<<EOT\)\));\s*$|^\s*BEGIN_TEXT\s*$/i ){
           last;
         }
     }
-    while(my $line = <$file>)  {
-        if ($line =~ /EOT|END_TEXT/i){
-            return $statement;
-        } else {
+    while(my $line = <$FILE>)  {
+        if (!($line =~ /EOT|END_TEXT/i)){
             $statement .= $line;
         }
     }
+    close $FILE;
+    $self->problem->statement($statement); 
 }
 
 sub parseTextbook {
